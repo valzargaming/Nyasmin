88621919e004faf87646344a6b504347ba6535b7
Charlotte Dunois <github@charuru.moe>
2017-10-16 22:15:59 +0200

    Final

--- Utils/Snowflake.php
+++ Utils/Snowflake.php
@@ -19,16 +19,16 @@ class Snowflake { //TODO: Docs
         $low = $snowflake & 0x00000000ffffffff;
         $binary = \pack('NN', $high, $low);
         
-        $binary = self::convertBase($snowflake, '0123456789', '01');
+        $binary = self::convertBase($snowflake, 10, 2);
         
         $binary = \str_pad($binary, 64, 0, STR_PAD_LEFT);
-        $timestamp = \round(self::convertBase(\substr($binary, 0, 42), '01', '0123456789') / 1000) + self::EPOCH;
+        $timestamp = \round(self::convertBase(\substr($binary, 0, 42), 2, 10) / 1000) + self::EPOCH;
 
         $this->data = array(
             'timestamp' => $timestamp,
-            'workerID' => (int) self::convertBase(\substr($binary, 42, 47), '01', '0123456789'),
-            'processID' => (int) self::convertBase(\substr($binary, 47, 52), '01', '0123456789'),
-            'increment' => (int) self::convertBase(\substr($binary, 52, 64), '01', '0123456789'),
+            'workerID' => (int) self::convertBase(\substr($binary, 42, 47), 2, 10),
+            'processID' => (int) self::convertBase(\substr($binary, 47, 52), 2, 10),
+            'increment' => (int) self::convertBase(\substr($binary, 52, 64), 2, 10),
             'binary' => $binary
         );
     }
@@ -43,10 +43,10 @@ class Snowflake { //TODO: Docs
         }
         
         $mtime = explode(' ', \microtime());
-        $time = ((string) ((int) $mtime[1] - self::EPOCH)).\substr($mtime[0], 2, 5);
+        $time = ((string) ((int) $mtime[1] - self::EPOCH)).\str_replace('.', '', \substr($mtime[0], 0, 3));
         
-        $binary = \str_pad(self::convertBase($time, '0123456789', '01'), 42, 0, STR_PAD_LEFT).'0000100000'.\str_pad(self::convertBase((self::$increment++), '0123456789', '01'), 12, 0, STR_PAD_LEFT);
-        return self::convertBase($binary, '01', '0123456789');
+        $binary = \str_pad(self::convertBase($time, 10, 2), 42, 0, STR_PAD_LEFT).'0000100000'.\str_pad(self::convertBase((self::$increment++), 10, 2), 12, 0, STR_PAD_LEFT);
+        return self::convertBase($binary, 2, 10);
     }
     
     function getTimestamp() {
@@ -78,40 +78,56 @@ class Snowflake { //TODO: Docs
             return $input;
         }
         
-        $numberLen = \strlen($input);
-        $fromLen = \strlen($fromBase);
-        $toLen = \strlen($toBase);
+        $fromBaseArr = self::getBase($fromBase);
+        $toBaseArr = self::getBase($toBase);
         
-        $fromBaseArr = \str_split($fromBase, 1);
-        $toBaseArr = \str_split($toBase, 1);
+        $numberLen = \strlen($input);
         $number = \str_split($input, 1);
         
         $retval = '';
         
-        if($toBase == '0123456789') {
+        if($toBase == 10) {
             $retval = 0;
             for ($i = 1; $i <= $numberLen; $i++) {
-                $retval = \bcadd($retval, \bcmul(\array_search($number[($i - 1)], $fromBaseArr), \bcpow($fromLen, ($numberLen - $i))));
+                $retval = \bcadd($retval, \bcmul(\array_search($number[($i - 1)], $fromBaseArr), \bcpow($fromBase, ($numberLen - $i))));
             }
             
             return $retval;
         }
         
-        if($fromBase != '0123456789') {
-            $base10 = self::convertBase($input, $fromBase, '0123456789');
+        if($fromBase != 10) {
+            $base10 = self::convertBase($input, $fromBase, 10);
         } else {
             $base10 = $input;
         }
         
-        if($base10 < $toLen) {
+        if($base10 < $toBase) {
             return $toBaseArr[$base10];
         }
         
         while($base10 != '0') {
-            $retval = $toBaseArr[\bcmod($base10, $toLen)].$retval;
-            $base10 = \bcdiv($base10, $toLen, 0);
+            $retval = $toBaseArr[\bcmod($base10, $toBase)].$retval;
+            $base10 = \bcdiv($base10, $toBase, 0);
         }
         
         return $retval;
+    }
+    
+    static private function getBase($base) {
+        switch((int) $base) {
+            case 2:
+                return array(0, 1);
+            break;
+            case 8:
+                return array(0, 1, 2, 3, 4, 5, 6, 7);
+            break;
+            default:
+            case 10:
+                return array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+            break;
+            case 16:
+                return array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F');
+            break;
+        }
     }
 }
