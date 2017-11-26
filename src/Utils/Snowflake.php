d42a6c36b5c1c6a3b595e3bfbcd5c97528c31bc0
Charlotte Dunois <github@charuru.moe>
2017-11-26 22:06:19 +0100

    Add bcmath to composer

--- src/Utils/Snowflake.php
+++ src/Utils/Snowflake.php
@@ -38,14 +38,14 @@ class Snowflake {
      * @param string $snowflake
      */
     function __construct(string $snowflake) {
-        $this->binary = \str_pad(self::convertBase($snowflake, 10, 2), 64, 0, STR_PAD_LEFT);
+        $this->binary = \str_pad(self::decimal2Binary($snowflake), 64, 0, \STR_PAD_LEFT);
         
-        $time = self::convertBase(\substr($this->binary, 0, 42), 2, 10);
+        $time = self::binary2Decimal(\substr($this->binary, 0, 42));
         
         $this->timestamp = (float) ((((int) \substr($time, 0, -3)) + self::EPOCH).'.'.\substr($time, -3));
-        $this->workerID = (int) self::convertBase(\substr($this->binary, 42, 5), 2, 10);
-        $this->processID = (int) self::convertBase(\substr($this->binary, 47, 5), 2, 10);
-        $this->increment = (int) self::convertBase(\substr($this->binary, 52, 12), 2, 10);
+        $this->workerID = (int) self::binary2Decimal(\substr($this->binary, 42, 5));
+        $this->processID = (int) self::binary2Decimal(\substr($this->binary, 47, 5));
+        $this->increment = (int) self::binary2Decimal(\substr($this->binary, 52, 12));
     }
     
     /**
@@ -89,8 +89,8 @@ class Snowflake {
         $mtime = \explode('.', (string) \microtime(true));
         $time = ((string) (((int) $mtime[0]) - self::EPOCH)).\substr($mtime[1], 0, 3);
         
-        $binary = \str_pad(self::convertBase($time, 10, 2), 42, 0, STR_PAD_LEFT).'0000100000'.\str_pad(self::convertBase((self::$incrementIndex++), 10, 2), 12, 0, STR_PAD_LEFT);
-        return self::convertBase($binary, 2, 10);
+        $binary = \str_pad(self::decimal2Binary($time), 42, 0, \STR_PAD_LEFT).'0000100000'.\str_pad(self::decimal2Binary((self::$incrementIndex++)), 12, 0, \STR_PAD_LEFT);
+        return self::binary2Decimal($binary);
     }
     
     /**
@@ -102,71 +102,35 @@ class Snowflake {
     }
     
     /**
-     * Converts numbers from one base to another.
+     * Converts numbers from base 10 to base 2.
      * @param string $input
-     * @param int    $fromBase
-     * @param int    $toBase
+     * @return string
      */
-    static function convertBase($input, $fromBase, $toBase) {
-        if($fromBase === $toBase) {
-            return $input;
+    function decimal2Binary(string $input) {
+        $binary = '';
+        
+        while($input != '0') {
+            $binary .= \chr(48 + ($input[\strlen($input) - 1] % 2));
+            $input = \bcdiv($input, 2);
         }
         
-        $fromBaseArr = self::getBaseArray($fromBase);
-        $toBaseArr = self::getBaseArray($toBase);
-        
-        $numberLen = \strlen($input);
-        $number = \str_split($input, 1);
-        
-        $retval = '';
-        
-        if($toBase === 10) {
-            $retval = 0;
-            for ($i = 1; $i <= $numberLen; $i++) {
-                $retval = \bcadd($retval, \bcmul(\array_search($number[($i - 1)], $fromBaseArr), \bcpow($fromBase, ($numberLen - $i))));
-            }
-            
-            return $retval;
-        }
-        
-        if($fromBase != 10) {
-            $base10 = self::convertBase($input, $fromBase, 10);
-        } else {
-            $base10 = $input;
-        }
-        
-        if($base10 < $toBase) {
-            return $toBaseArr[$base10];
-        }
-        
-        while($base10 != 0) {
-            $retval = $toBaseArr[\bcmod($base10, $toBase)].$retval;
-            $base10 = \bcdiv($base10, $toBase, 0);
-        }
-        
-        return $retval;
+        $binary = \strrev($binary);
+        return $binary;
     }
     
     /**
-     * Return the valid values for a given base.
-     * @param string|int $base
-     * @return array
+     * Converts numbers from base 2 to base 10.
+     * @param string $input
+     * @return string
      */
-    static private function getBaseArray($base) {
-        switch((int) $base) {
-            case 2:
-                return array(0, 1);
-            break;
-            case 8:
-                return array(0, 1, 2, 3, 4, 5, 6, 7);
-            break;
-            default:
-            case 10:
-                return array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-            break;
-            case 16:
-                return array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F');
-            break;
+    function binary2Decimal(string $input) {
+        $decimal = '';
+
+        $leni = \strlen($input);
+        for($i = 0; $i < $leni; $i++) {
+            $decimal = \bcadd(\bcmul($decimal, 2), $input[$i]);
         }
+        
+        return $decimal;
     }
 }
