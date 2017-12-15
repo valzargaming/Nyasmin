0c7c9e6e79f54e2b07235f313950e3efd4a5fdd8
Charlotte Dunois <github@charuru.moe>
2017-12-15 23:40:33 +0100

    Bump version

--- src/Constants.php
+++ src/Constants.php
@@ -17,7 +17,7 @@ class Constants {
      * The version of Yasmin.
      * @var string
      */
-    const VERSION = '0.1.4';
+    const VERSION = '0.1.5';
     
     /**
      * The default HTTP user agent.
