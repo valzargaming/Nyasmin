a9a3bf3d14853f9b78f8d6c0cbfecd2b6ee8429a
Charlotte Dunois <github@charuru.moe>
2018-03-06 17:35:01 +0100

    Bump version for master

--- src/Client.php
+++ src/Client.php
@@ -31,7 +31,7 @@ class Client implements \CharlotteDunois\Events\EventEmitterInterface {
      * The version of Yasmin.
      * @var string
      */
-    const VERSION = '0.3.0';
+    const VERSION = '0.3.0-master';
     
     /**
      * WS connection status: Disconnected.
