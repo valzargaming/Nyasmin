20de6a7fcac8c72e0992a5f14d74e7bf307b770b
Charlotte Dunois <github@charuru.moe>
2018-07-02 23:17:22 +0200

    Typo

--- src/Interfaces/RatelimitBucketInterface.php
+++ src/Interfaces/RatelimitBucketInterface.php
@@ -17,7 +17,8 @@ namespace CharlotteDunois\Yasmin\Interfaces;
  *  * In memory ratelimit bucket, using arrays - Class: <code>\CharlotteDunois\Yasmin\HTTP\RatelimitBucket</code> (default)<br>
  *  * Redis ratelimit bucket, using Athena to interface with Redis - Class: <code>\CharlotteDunois\Yasmin\HTTP\AthenaRatelimitBucket</code>
  *
- * To use a different one then the default, you have to pass the full qualified class name to the client constructor as client option <code>http.ratelimitbucket.name</code>.<br>
+ * To use a different one than the default, you have to pass the full qualified class name to the client constructor as client option <code>http.ratelimitbucket.name</code>.
+ *
  * The Redis ratelimit bucket system uses Athena, an asynchronous redis cache for PHP. The package is called <code>charlottedunois/athena</code> (which is suggested on composer).<br>
  * To be able to use the Redis ratelimit bucket, you need to pass an instance of <code>AthenaCache</code> as client option <code>http.ratelimitbucket.athena</code> to the client.
  */
