979403faeb3f4ea2cbd3fba95b707f1daa6e1723
Charlotte Dunois <github@charuru.moe>
2019-01-01 17:24:40 +0100

    Update comment header

--- src/Traits/TextChannelTrait.php
+++ src/Traits/TextChannelTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Yasmin
- * Copyright 2017-2018 Charlotte Dunois, All Rights Reserved
+ * Copyright 2017-2019 Charlotte Dunois, All Rights Reserved
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
