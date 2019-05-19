valzargaming/Nyasmin

commit d55789860dcc760d4c63133d277109c822459daa

Check for empty values in MessageEmbed as well as violations of the 6000-char limit (#104)

src/Models/MessageEmbed.php
@@ -114,25 +114,31 @@ class MessageEmbed extends Base {
     function __construct(array $embed = array()) {
         if(!empty($embed)) {
             $this->type = $embed['type'] ?? 'rich';
-            $this->title = $embed['title'] ?? null;
+            
+            if(!empty($embed['title'])) {
+                $this->setTitle($embed['title']);
+            }
             
             if(!empty($embed['author'])) {
-                $this->author = array(
-                    'name' => ((string) ($embed['author']['name'] ?? '')),
-                    'icon' => ((string) ($embed['author']['icon_url'] ?? '')),
-                    'url' => ((string) ($embed['author']['url'] ?? ''))
+                $this->setAuthor(
+                    ((string) ($embed['author']['name'] ?? '')),
+                    ((string) ($embed['author']['icon_url'] ?? '')),
+                    ((string) ($embed['author']['url'] ?? ''))
                 );
             }
             
-            $this->description = $embed['description'] ?? null;
+            if(!empty($embed['description'])) {
+                $this->setDescription($embed['description']);
+            }
+            
             $this->url = $embed['url'] ?? null;
             $this->timestamp = (!empty($embed['timestamp']) ? (new \DateTime($embed['timestamp']))->getTimestamp() : null);
             $this->color = $embed['color'] ?? null;
             
             if(!empty($embed['footer'])) {
-                $this->footer = array(
-                    'text' => ((string) ($embed['footer']['text'] ?? '')),
-                    'icon' => ((string) ($embed['footer']['icon_url'] ?? ''))
+                $this->setFooter(
+                    ((string) ($embed['footer']['text'] ?? '')),
+                    ((string) ($embed['footer']['icon_url'] ?? ''))
                 );
             }
             
@@ -167,7 +173,13 @@ class MessageEmbed extends Base {
                 );
             }
             
-            $this->fields = $embed['fields'] ?? array();
+            foreach(($embed['fields'] ?? array()) as $field) {
+                $this->addField(
+                    ((string) ($field['name'] ?? '')),
+                    ((string) ($embed['value'] ?? '')),
+                    ((bool) ($embed['inline'] ?? false))
+                );
+            }
         }
     }
     
@@ -202,8 +214,17 @@ class MessageEmbed extends Base {
     function setAuthor($name, string $iconurl = '', string $url = '') {
         $name = (string) $name;
         
+        if(\mb_strlen($name) === 0) {
+            $this->author = null;
+            return $this;
+        }
+        
         if(\mb_strlen($name) > 256) {
             throw new \InvalidArgumentException('Author name can not be longer than 256 characters.');
+        }
+        
+        if($this->exceedsOverallLimit(\mb_strlen($name))) {
+            throw new \InvalidArgumentException('Embed text values collectively can not exceed than 6000 characters');
         }
         
         $this->author = array(
@@ -236,8 +257,17 @@ class MessageEmbed extends Base {
     function setDescription($description) {
         $description = (string) $description;
         
+        if(\mb_strlen($description) === 0) {
+            $this->description = null;
+            return $this;
+        }
+        
         if(\mb_strlen($description) > 2048) {
             throw new \InvalidArgumentException('Embed description can not be longer than 2048 characters');
+        }
+        
+        if($this->exceedsOverallLimit(\mb_strlen($description))) {
+            throw new \InvalidArgumentException('Embed text values collectively can not exceed than 6000 characters');
         }
         
         $this->description = $description;
@@ -254,8 +284,17 @@ class MessageEmbed extends Base {
     function setFooter($text, string $iconurl = '') {
         $text = (string) $text;
         
+        if(\mb_strlen($text) === 0) {
+            $this->footer = null;
+            return $this;
+        }
+        
         if(\mb_strlen($text) > 2048) {
             throw new \InvalidArgumentException('Footer text can not be longer than 2048 characters.');
+        }
+        
+        if($this->exceedsOverallLimit(\mb_strlen($text))) {
+            throw new \InvalidArgumentException('Embed text values collectively can not exceed than 6000 characters');
         }
         
         $this->footer = array(
@@ -303,8 +342,17 @@ class MessageEmbed extends Base {
      * @throws \InvalidArgumentException
      */
     function setTitle(string $title) {
+        if(\mb_strlen($title) == 0) {
+            $this->title = null;
+            return $this;
+        }
+        
         if(\mb_strlen($title) > 256) {
             throw new \InvalidArgumentException('Embed title can not be longer than 256 characters');
+        }
+        
+        if($this->exceedsOverallLimit(\mb_strlen($title))) {
+            throw new \InvalidArgumentException('Embed text values collectively can not exceed than 6000 characters');
         }
         
         $this->title = $title;
@@ -338,12 +386,20 @@ class MessageEmbed extends Base {
         $title = (string) $title;
         $value = (string) $value;
         
+        if(\mb_strlen($title) === 0 || \mb_strlen($value) === 0) {
+            throw new \InvalidArgumentException('Both embed title and value must not be empty strings');
+        }
+        
         if(\mb_strlen($title) > 256) {
             throw new \InvalidArgumentException('Embed title can not be longer than 256 characters');
         }
         
         if(\mb_strlen($value) > 1024) {
             throw new \InvalidArgumentException('Embed value can not be longer than 1024 characters');
+        }
+        
+        if($this->exceedsOverallLimit((\mb_strlen($title) + \mb_strlen($value)))) {
+            throw new \InvalidArgumentException('Embed text values collectively can not exceed than 6000 characters');
         }
         
         $this->fields[] = array(
@@ -353,5 +409,26 @@ class MessageEmbed extends Base {
         );
         
         return $this;
+    }
+    
+    /**
+     * Checks to see if adding a property has put us over Discord's 6000-char overall limit.
+     * @return bool
+     */
+    protected function exceedsOverallLimit(int $addition): bool {
+        $total = (
+            \mb_strlen(($this->title ?? "")) +
+            \mb_strlen(($this->description ?? "")) +
+            \mb_strlen(($this->footer['text'] ?? "")) +
+            \mb_strlen(($this->author['name'] ?? "")) +
+            $addition
+        );
+        
+        foreach($this->fields as $field) {
+            $total += \mb_strlen($field['name']);
+            $total += \mb_strlen($field['value']);
+        }
+        
+        return ($total > 6000);
     }
 }

commit 2263a71d21be4a9bb8506bcdb754950b162c2098

Fix bug in MessageEmbed, introduced with #104

.gitignore
@@ -1,3 +1,5 @@
+.idea
+.idea/*
 cache
 cache/*
 sami/*

src/Models/Base.php
@@ -19,6 +19,7 @@ abstract class Base implements \JsonSerializable, \Serializable {
      */
     function __construct() {
         // We don't have anything to do.
+        echo 'Constructing '.get_class($this).'#'.spl_object_hash($this).PHP_EOL;
     }
     
     /**
@@ -27,6 +28,7 @@ abstract class Base implements \JsonSerializable, \Serializable {
      */
     function __destruct() {
         $this->_markForDelete();
+        echo 'Destructing '.get_class($this).'#'.spl_object_hash($this).PHP_EOL;
     }
     
     /**

src/Models/MessageEmbed.php
@@ -110,6 +110,7 @@ class MessageEmbed extends Base {
     /**
      * Constructs a new instance.
      * @param array  $embed
+     * @throws \Throwable
      */
     function __construct(array $embed = array()) {
         if(!empty($embed)) {
@@ -176,8 +177,8 @@ class MessageEmbed extends Base {
             foreach(($embed['fields'] ?? array()) as $field) {
                 $this->addField(
                     ((string) ($field['name'] ?? '')),
-                    ((string) ($embed['value'] ?? '')),
-                    ((bool) ($embed['inline'] ?? false))
+                    ((string) ($field['value'] ?? '')),
+                    ((bool) ($field['inline'] ?? false))
                 );
             }
         }
@@ -186,6 +187,7 @@ class MessageEmbed extends Base {
     /**
      * {@inheritdoc}
      * @return mixed
+     * @throws \Exception
      * @throws \RuntimeException
      * @internal
      */
@@ -214,7 +216,7 @@ class MessageEmbed extends Base {
     function setAuthor($name, string $iconurl = '', string $url = '') {
         $name = (string) $name;
         
-        if(\mb_strlen($name) === 0) {
+        if(\strlen($name) === 0) {
             $this->author = null;
             return $this;
         }
@@ -250,14 +252,14 @@ class MessageEmbed extends Base {
     
     /**
      * Set the description of this embed.
-     * @param string  $description  Maxiumum length is 2048 characters.
+     * @param string  $description  Maximum length is 2048 characters.
      * @return $this
      * @throws \InvalidArgumentException
      */
     function setDescription($description) {
         $description = (string) $description;
         
-        if(\mb_strlen($description) === 0) {
+        if(\strlen($description) === 0) {
             $this->description = null;
             return $this;
         }
@@ -284,7 +286,7 @@ class MessageEmbed extends Base {
     function setFooter($text, string $iconurl = '') {
         $text = (string) $text;
         
-        if(\mb_strlen($text) === 0) {
+        if(\strlen($text) === 0) {
             $this->footer = null;
             return $this;
         }
@@ -329,6 +331,7 @@ class MessageEmbed extends Base {
      * Set the timestamp of this embed.
      * @param int|null  $timestamp
      * @return $this
+     * @throws \Exception
      */
     function setTimestamp(?int $timestamp = null) {
         $this->timestamp = (new \DateTime(($timestamp !== null ? '@'.$timestamp : 'now')))->format('c');
@@ -342,7 +345,7 @@ class MessageEmbed extends Base {
      * @throws \InvalidArgumentException
      */
     function setTitle(string $title) {
-        if(\mb_strlen($title) == 0) {
+        if(\strlen($title) == 0) {
             $this->title = null;
             return $this;
         }
@@ -386,7 +389,7 @@ class MessageEmbed extends Base {
         $title = (string) $title;
         $value = (string) $value;
         
-        if(\mb_strlen($title) === 0 || \mb_strlen($value) === 0) {
+        if(\strlen($title) === 0 || \strlen($value) === 0) {
             throw new \InvalidArgumentException('Both embed title and value must not be empty strings');
         }
         
@@ -413,6 +416,7 @@ class MessageEmbed extends Base {
     
     /**
      * Checks to see if adding a property has put us over Discord's 6000-char overall limit.
+     * @param int  $addition
      * @return bool
      */
     protected function exceedsOverallLimit(int $addition): bool {