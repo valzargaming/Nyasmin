3beff0ee55e86ea44420cfd4942e722542308266
Charlotte Dunois <github@charuru.moe>
2019-12-01 13:05:50 +0100

    Update version

--- src/Client.php
+++ src/Client.php
@@ -34,7 +34,7 @@ class Client implements \CharlotteDunois\Events\EventEmitterInterface {
      * The version of Yasmin.
      * @var string
      */
-    const VERSION = '0.6.4-dev';
+    const VERSION = '0.7.0';
     
     /**
      * WS connection status: Disconnected.
