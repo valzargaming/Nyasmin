da3f2cad083638d50eaae967839bdd0ff8e73b02
Valithor Obsidion <valzargaming@gmail.com>
2020-01-17 17:51:06 -0500

    Logging changes to cached messages
    Messages that existed before the bot was started will not have changes logged. In the future, a method to compare the new message's timestamp with $timestampSetup and skip messages with a timestamp older than $timestampSetup. This will be a more definite way to know in advance if nothing will be found in the message cache.

--- run.php
+++ run.php
@@ -55,8 +55,10 @@ $discord->once('ready', function () use ($discord){	// Listen for events here
 			'afk' => false
 		)
 	);
-	
 	echo "BOT IS READY" . PHP_EOL;
+	$timestampSetup = time();
+	echo "timestampSetup: " . $timestampSetup . PHP_EOL;
+	//Save this to a file to be loaded, used in messageUpdate
 	
 	$discord->on('message', function ($message){ //Handling of a message
 		$message_content = $message->content;
@@ -1865,8 +1867,108 @@ $discord->once('ready', function () use ($discord){	// Listen for events here
 	$discord->on('messageUpdate', function ($message_new, $message_old){ //Handling of a message being changed
 		echo "messageUpdate" . PHP_EOL;
 		//This event listener gets triggered willy-nilly so we need to do some checks here if we want to get anything useful out of it
-		$message_content_new = $message_new->content;
-		$message_content_old = $message_old->content;
+		//TODO: If the timestamp is older than timestampSetup nothing will be passed to this method, so we should log that *something* changed
+		
+		$message_content_new = $message_new->content; //Null if message is too old
+		$message_content_old = $message_old->content; //Null if message is too old
+		$message_id_new = $message_new->id; //This doesn't match any message id if the message is too old
+		
+		//Only process message changes
+		if ($message_content_new === $message_content_old){
+			echo "NO MESSAGE CONTENT CHANGE OR MESSAGE TOO OLD" . PHP_EOL;
+			return;
+		}
+		
+		//Make sure the messages aren't blank
+		if (($message_content_new == NULL) || ($message_content_new == "")) { //This should never trigger, but just in case...
+			echo "BLANK OR OLD MESSAGE EDITED" . PHP_EOL;
+			return;
+		}
+
+		/*
+		Debug output
+		
+//		Check the timestamp
+		$createdTimestamp_new = $message_new->createdTimestamp;
+		$createdTimestamp_old = $message_old->createdTimestamp;
+		
+		$editedTimestamp_new = $message_new->editedTimestamp;
+		$editedTimestamp_old = $message_old->editedTimestamp;
+		
+		echo "message_content_new: " . $message_content_new . PHP_EOL;
+		echo "message_content_old: " . $message_content_old . PHP_EOL;
+		echo PHP_EOL;
+		echo "createdTimestamp_new: " . $createdTimestamp_new . PHP_EOL;
+		echo "createdTimestamp_old: " . $createdTimestamp_old . PHP_EOL;
+		echo PHP_EOL;
+		echo "editedTimestamp_new: " . $editedTimestamp_new . PHP_EOL;
+		echo "editedTimestamp_old: " . $editedTimestamp_old . PHP_EOL;
+		echo PHP_EOL;
+		*/
+		
+		//Load global variables
+		$guild = $message_new->guild;
+		GLOBAL $modlog_channel_id;
+		$modlog_channel	= $guild->channels->get($modlog_channel_id);
+		
+		//Load author info
+		$author_user				= $message_new->author; //User object
+		$author_channel 			= $message_new->channel;
+		$author_channel_id			= $author_channel->id; 												//echo "author_channel_id: " . $author_channel_id . PHP_EOL;
+		$author_channel_class		= get_class($author_channel);
+		$is_dm = false;
+		if ($author_channel_class === "CharlotteDunois\Yasmin\Models\DMChannel"){ //True if direct message
+			$is_dm = true;
+			return; //Don't try and process direct messages
+		}
+		$author_username 			= $author_user->username; 											//echo "author_username: " . $author_username . PHP_EOL;
+		$author_discriminator 		= $author_user->discriminator;										//echo "author_discriminator: " . $author_discriminator . PHP_EOL;
+		$author_id 					= $author_user->id;													//echo "author_id: " . $author_id . PHP_EOL;
+		$author_avatar 				= $author_user->getAvatarURL();										//echo "author_avatar: " . $author_avatar . PHP_EOL;
+		$author_check 				= "$author_username#$author_discriminator"; 						//echo "author_check: " . $author_check . PHP_EOL;
+		
+		$changes = "";
+		if (($message_content_new != $message_content_old) || (($message_content_old == "") || ($message_content_old == NULL))) 	{		
+			
+//			Log the change
+			echo "message_content_new: " . $message_content_new . PHP_EOL;
+			echo "message_content_old: " . $message_content_old . PHP_EOL;
+			
+//			Build the string for the reply
+			$old_role_name_queue 									= substr($old_role_name_queue, 0, -1);
+			$old_role_name_queue_full 								= $old_role_name_queue_full . PHP_EOL . $old_role_name_queue;
+			$changes = $changes . "**Message ID:**: $message_id_new\n";
+			$changes = $changes . "**Before:** $message_content_old\n";
+			$changes = $changes . "**After:** $message_content_new\n";
+
+		}
+		if ($changes != ""){
+			//Build the embed
+			//$changes = "**Message edit**:\n" . $changes;
+			
+//			Build the embed message
+			$embed = new \CharlotteDunois\Yasmin\Models\MessageEmbed();
+			$embed
+//				->setTitle("Commands")																	// Set a title
+				->setColor("a7c5fd")																	// Set a color (the thing on the left side)
+//				->setDescription("Commands for Blue's Cloudy Palace")									// Set a description (below title, above fields)
+				->addField("Message Update", "$changes")												// New line after this
+				
+//				->setThumbnail("$author_avatar")														// Set a thumbnail (the image in the top right corner)
+//				->setImage('https://avatars1.githubusercontent.com/u/4529744?s=460&v=4')             	// Set an image (below everything except footer)
+				->setTimestamp()                                                                     	// Set a timestamp (gets shown next to footer)
+				->setAuthor("$author_check ($author_id)", "$author_avatar")  							// Set an author with icon
+				->setFooter("Palace Bot by Valithor#5947")                             					// Set a footer without icon
+				->setURL("");                             												// Set the URL
+//			Send the message
+//			We do not need another promise here, so we call done, because we want to consume the promise
+			$modlog_channel->send('', array('embed' => $embed))->done(null, function ($error){
+				echo $error.PHP_EOL; //Echo any errors
+			});
+			return true;
+		}else{ //No info we want to check was changed
+			return true;
+		}
 	});
 	
 	$discord->on('messageDelete', function ($message){ //Handling of a message being deleted
@@ -1876,7 +1978,7 @@ $discord->once('ready', function () use ($discord){	// Listen for events here
 		$message_content												= $message->content;
 		$message_id														= $message->id;
 		if ( ($message_content == NULL) || ($message_content == "") ){
-			echo "BLANK MESSAGE" . PHP_EOL;
+			echo "BLANK MESSAGE DELETED" . PHP_EOL;
 			return true;
 		}			//Don't process blank messages, bots, or webhooks
 		$message_content_lower											= strtolower($message_content);
