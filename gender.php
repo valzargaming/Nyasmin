47427d8642e60b61eb8d77aa24ca601146632589
Valithor Obsidion <valzargaming@gmail.com>
2020-01-18 16:47:59 -0500

    Update gender.php

--- gender.php
+++ gender.php
@@ -1,8 +1,9 @@
 <?php
+include_once 'custom_functions.php';
 $gender_message_id_default = "";
 if(CheckFile(null, "gender_message_id.php"))	$gender_message_id = VarLoad(null, "gender_message_id.php");			//Load saved option file
 else{
-	gender_message_id = $gender_message_id_default;
+	$gender_message_id = $gender_message_id_default;
 	VarSave(null, "gender_message_id.php", $gender_message_id);
 }
 
@@ -12,7 +13,7 @@ $female = "♀️";
 $male = "♂️";
 
 //Message copy-pasta:
-$gender_message_text: = "**Role Menu: Gender**
+$gender_message_text = "**Role Menu: Gender**
 :droplet: : `Gender Fluid`
 :no_entry: : `Non Binary`
 :female_sign: : `Female`
