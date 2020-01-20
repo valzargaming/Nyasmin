810b9247353777a2b9472f5dc68a9988c56c2f07
Valithor Obsidion <valzargaming@gmail.com>
2020-01-20 13:27:40 -0500

    Template file for anyone to create their own roles

---
+++ custom_roles.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'custom_functions.php';
+
+$custom_roles = array(
+	"role Name" => "unicode",
+	"another Role Name" => "unicode"
+);
+
+//Message copy-pasta:
+$gender_message_text = "**Role Menu: Custom**
+:emoji: : `Role Name`
+:emoji: : `Another Role Name`
+";
+?>
