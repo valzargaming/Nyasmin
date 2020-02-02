f5490d161d8fd81082525d1167a7d6c6109ce35e
Valithor Obsidion <valzargaming@gmail.com>
2020-02-02 16:11:37 -0500

    Update custom_roles.php

--- custom_roles.php
+++ custom_roles.php
@@ -1,13 +1,13 @@
 <?php
 include_once 'custom_functions.php';
 
-$custom_roles = array(
+$customroles = array(
 	"role Name" => "unicode",
 	"another Role Name" => "unicode"
 );
 
 //Message copy-pasta:
-$custom_message_text = "**Role Menu: Custom**
+$customroles_message_text = "**Role Menu: Custom**
 :emoji: : `Role Name`
 :emoji: : `Another Role Name`
 ";
