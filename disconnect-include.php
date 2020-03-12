32f4957000d4de51f591135e8d980f3a311857e7
Valithor Obsidion <valzargaming@gmail.com>
2020-03-12 19:32:23 -0400

    x2

--- disconnect-include.php
+++ disconnect-include.php
@@ -6,5 +6,8 @@ echo "----- BOT DISCONNECTED FROM DISCORD WITH CODE $code FOR REASON: $erMsg ---
 
 echo "RESTARTING LOOP";
 $loop->stop();
+$discord->login($token)->done(null, function ($error){
+		echo "[LOGIN ERROR] $error".PHP_EOL; //Echo any errors
+	});
 $loop->run();
 ?>
