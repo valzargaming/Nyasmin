7e8d0df26efa68dfb54c59a442671c4895fa5170
Valithor Obsidion <valzargaming@gmail.com>
2020-04-02 23:44:07 -0400

    Hotfix for random disconnects
    Not yet tested. $packet can sometimes be null and cause a runtime error. It should be safe to assume in this case that a reconnect should be attempted.

--- src/WebSocket/Handlers/Reconnect.php
+++ src/WebSocket/Handlers/Reconnect.php
@@ -21,6 +21,8 @@ class Reconnect implements \CharlotteDunois\Yasmin\Interfaces\WSHandlerInterface
     }
     
     function handle(\CharlotteDunois\Yasmin\WebSocket\WSConnection $ws, $packet): void {
+		if ($packet['d'] === NULL) $packet['d'] = true; //Hotfix for random disconnects
         $ws->reconnect($packet['d']);
     }
 }
+?>
