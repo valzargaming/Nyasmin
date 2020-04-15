ea167c881a67e8fc7049d5beb85cc8fa6c00fe03
Valithor Obsidion <valzargaming@gmail.com>
2020-04-15 15:50:44 -0400

    Fixes high CPU usage issue after reconnect

--- disconnect-include.php
+++ disconnect-include.php
@@ -4,10 +4,16 @@ echo "----- BOT DISCONNECTED FROM DISCORD WITH CODE $code FOR REASON: $erMsg ---
 //$restart_cmd = 'cmd /c "'. __DIR__  . '\run.bat"'; //echo $restart_cmd . PHP_EOL;
 //system($restart_cmd);
 
-echo "RESTARTING LOOP";
-$loop->stop();
+
+echo "[RESTART LOOP]" . PHP_EOL;
+$dt = new DateTime("now");  // convert UNIX timestamp to PHP DateTime
+echo "[TIME] " . $dt->format('d-m-Y H:i:s') . PHP_EOL; // output = 2017-01-01 00:00:00
+//$loop->stop();
+$discord->destroy();
+$discord = new \CharlotteDunois\Yasmin\Client(array(), $loop);
 $discord->login($token)->done(null, function ($error){
-		echo "[LOGIN ERROR] $error".PHP_EOL; //Echo any errors
-	});
-$loop->run();
+	echo "[LOGIN ERROR] $error".PHP_EOL; //Echo any errors
+});
+//$loop->run();
+
 ?>
