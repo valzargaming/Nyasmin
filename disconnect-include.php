91b6e87f802ba39f6a1362daf43b54ac429a2c28
Valithor Obsidion <valzargaming@gmail.com>
2020-04-22 17:15:26 -0400

    In-line documentation

--- disconnect-include.php
+++ disconnect-include.php
@@ -1,19 +1,18 @@
 <?php
 echo "----- BOT DISCONNECTED FROM DISCORD WITH CODE $code FOR REASON: $erMsg -----" . PHP_EOL;
-//echo "RESTARTING BOT" . PHP_EOL;
-//$restart_cmd = 'cmd /c "'. __DIR__  . '\run.bat"'; //echo $restart_cmd . PHP_EOL;
-//system($restart_cmd);
-
+/*This restarts the entire bot, tested working on Win10 and Win8.1. It's not recommended because you'll lose your cache and active loop.
+echo "[RESTARTING]" . PHP_EOL;
+$restart_cmd = 'cmd /c "'. __DIR__  . '\run.bat"'; //echo $restart_cmd . PHP_EOL;
+system($restart_cmd);
+*/
 
 echo "[RESTART LOOP]" . PHP_EOL;
 $dt = new DateTime("now");  // convert UNIX timestamp to PHP DateTime
 echo "[TIME] " . $dt->format('d-m-Y H:i:s') . PHP_EOL; // output = 2017-01-01 00:00:00
-//$loop->stop();
 $discord->destroy();
-$discord = new \CharlotteDunois\Yasmin\Client(array(), $loop);
+$discord = new \CharlotteDunois\Yasmin\Client(array(), $loop); //Create a new client using the same React loop
 $discord->login($token)->done(null, function ($error){
 	echo "[LOGIN ERROR] $error".PHP_EOL; //Echo any errors
 });
-//$loop->run();
 
 ?>
