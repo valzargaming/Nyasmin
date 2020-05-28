74cf4fc7f0998c7641382b62b30072b067afda8c
Valithor Obsidion <valzargaming@gmail.com>
2020-05-28 07:36:32 -0400

    image URLs

--- src/Models/GroupDMChannel.php
+++ src/Models/GroupDMChannel.php
@@ -60,7 +60,8 @@ class GroupDMChannel extends DMChannel implements \CharlotteDunois\Yasmin\Interf
      */
     function getIconURL(?int $size = null, string $format = 'png') {
         if ($this->icon !== null) {
-            return \CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['url'].\CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(\CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['channelicons'], $this->id, $this->icon, $format).(!empty($size) ? '?size='.$size : '');
+            $iconurl = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['url'].\CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(\CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['channelicons'], $this->id, $this->icon, $format).(!empty($size) ? '?size='.$size : '');
+            return str_replace("cdn.discord.com", "cdn.discordapp.com", $iconurl);
         }
         
         return null;
